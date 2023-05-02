3e2c2ebb10549de0160775d9ebc6df33e3d3ae5e
leesijin <@DESKTOP-B458PD1>
2023-05-02 20:21:04 +0900

    초기 커밋.

---
+++ middle project/page.h
@@ -0,0 +1,9 @@
+class Page {
+    public:
+        Page{int X, int Y, int Width, int Height, int Id, char Content}:x(X),y
+    private:
+        int x, y; // position of the page on the board
+        int width, height; // width and height of the page 
+        int id; // unique id for each page
+        char content; 
+};
