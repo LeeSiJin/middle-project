53b0bdf0ff7bc54ddbefecd6cb7b6540755d792c
leesijin <@DESKTOP-B458PD1>
2023-05-02 20:25:18 +0900

    Page constructor 완성

--- middle project/page.h
+++ middle project/page.h
@@ -1,9 +1,11 @@
 class Page {
     public:
-        Page{int X, int Y, int Width, int Height, int Id, char Content}:x(X),y
+        Page{int X, int Y, int Width, int Height, int Id, char Content}:x(X),y(Y),width(Width),height(Height),id(Id),content(Content)
+        {}
     private:
         int x, y; // position of the page on the board
         int width, height; // width and height of the page 
         int id; // unique id for each page
         char content; 
 };
+
